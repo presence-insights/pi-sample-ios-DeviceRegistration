8fb7a5de83b147d18c84383d42ff99705e4f6693
Daniel Choi <udchoi@us.ibm.com>
2015-10-27 14:44:21 -0400

    adding all the files

--- PIDeviceRegistration/ViewController.swift
+++ PIDeviceRegistration/ViewController.swift
@@ -1,25 +1,280 @@
+//   © Copyright 2015 IBM Corp.
 //
-//  ViewController.swift
-//  PIDeviceRegistration
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
 //
-//  Created by Daniel Choi on 8/19/15.
-//  Copyright (c) 2015 Daniel Choi. All rights reserved.
+//   http://www.apache.org/licenses/LICENSE-2.0
 //
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
+
 
 import UIKit
+import PresenceInsightsSDK
 
-class ViewController: UIViewController {
+
+class ViewController: UIViewController, UITextFieldDelegate {
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.deviceName.delegate = self
+        self.deviceType.delegate = self
+        self.unencryptedDataValue.delegate = self
+        self.unencryptedDataKey.delegate = self
+        self.datakey.delegate = self
+        self.datavalue.delegate = self
+        //creating a PIAdapter object with bm information
+        piAdapter = PIAdapter(tenant: tenantID,
+            org: orgID,
+            username: username,
+            password: passwd)
+        //piAdapter.enableLogging()
+        deviceType.userInteractionEnabled = false
+        
+        //initializing device to see if it's registered when the app is loaded.
+        //if regsitered switch the registered switch to on.
+        // note: i do not pull the encrypted data and unecnrypted data in the fields when the device is populated.
+        device = PIDevice(name: " ")
+        piAdapter.getDeviceByDescriptor(device.descriptor) { (rdevice, NSError) -> () in
+            if((rdevice.name?.isEmpty) == nil){
+                
+            }
+            else{
+                //using UI thread to make teh necessary changes.
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    self.RegisterSwitch.setOn(true, animated: true)
+                    self.deviceName.text = rdevice.name
+                    self.deviceType.text = rdevice.type
+                    self.alert("Status", messageInput: "The Device is currently registered. Device Name and Type will popular based on PI information")
+                })
+                
+                print("Device is already registered")
+            }
+        }
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    //hard coding the tenantID, orgID, username, password
+    //This information can be found in your Presence Insights UI/Dashboard
+    let tenantID = "XXXXXXXXX"
+    let orgID = "XXXXXXXXX"
+    let username = "XXXXXXXXX"
+    let passwd = "XXXXXXXXX"
+    
+    //initializing piAdapter object and PIdevice
+    var piAdapter : PIAdapter!
+    var device : PIDevice!
 
+    
+    @IBOutlet weak var deviceName: UITextField!
+    @IBOutlet weak var datakey: UITextField!
+    @IBOutlet weak var datavalue: UITextField!
+    @IBOutlet weak var unencryptedDataKey: UITextField!
+    @IBOutlet weak var unencryptedDataValue: UITextField!
+    @IBOutlet weak var deviceType: UITextField!
 
+    
+    //pop up alert and display BM information
+    @IBAction func bmAction(sender: UIButton) {
+        alert("BM Information", messageInput: "Username: \(username) \n Password: \(passwd) \n Tenant ID: \(tenantID) \n Org ID: \(orgID)")
+       
+    }
+
+    //UI selection for which device Type exist in the user's PI
+    @IBAction func DeviceTypeAction() {
+        //gets org information
+        piAdapter.getOrg { (org, NSError) -> () in
+            //grab the different device types
+            let Types = org.registrationTypes
+            //need this dispatch to change from backend thread to UI thread
+            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                let alert = UIAlertController(title: "Select Device Type", message: "", preferredStyle: UIAlertControllerStyle.Alert)
+                for Type in Types{
+                    alert.addAction(UIAlertAction(title: "\(Type)", style: UIAlertActionStyle.Default, handler:{ (UIAlertAction) in
+                        self.deviceType.text = Type
+                    }))
+                }
+                self.presentViewController(alert, animated: true, completion: nil)
+            })
+
+        }
+        
+    }
+    
+
+    
+    //Update action
+    @IBAction func Action(sender: UIButton) {
+        //adding device name to the device object
+        device = PIDevice(name: deviceName.text!)
+        device.type = deviceType.text
+
+        
+        //adding device type to the device object. 
+        //NOTE: the type has to exist in the Presence Insights. Default types are : "Internal" and "External"
+        
+        //method to add encrypted data
+        // ("VALUE", key: "KEY_NAME")
+        
+        //checks to see if key or value is empty and throw error as necessary
+        if( MissingText(datavalue, key: datakey) ==  false){
+            device.addToDataObject(datavalue.text!, key: datakey.text!)
+        }
+        else{
+            //RegisterSwitch.setOn(false, animated: true)
+            alert("Error", messageInput: "Check your Encrypted Data Key and Value Fields")
+        }
+
+        
+        //checks to see if key or value is empty and throw error as necessary for unencrypted data
+        if( MissingText(unencryptedDataValue, key: unencryptedDataKey) ==  false){
+            device.addToUnencryptedDataObject(unencryptedDataValue.text!, key: unencryptedDataKey.text!)
+        }
+        else{
+            //RegisterSwitch.setOn(false, animated: true)
+            alert("Error", messageInput: "Check your Unencrypted Data Key and Value Fields")
+        }
+        
+        
+        
+        //when the user flicks the switch to green
+        //checking to see if the device is registered before updating the device.
+        if RegisterSwitch.on {
+            
+            //set device register to true if the light is on
+            device.registered=true
+            //device is registered and will update.
+            piAdapter.updateDevice(device, callback: { (newDevice, NSError) -> () in
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    if(NSError == nil){
+                        self.alert("Success", messageInput: "Successfully Updated the Device Information")
+                    }
+                    else{
+                        self.alert("Error", messageInput: "\(NSError)")
+                    }
+                })
+            })
+
+            
+            
+        }
+        else {
+            //if the device is not registered, will alert saying must register device
+            alert("Error", messageInput: "Must Register Device Before Updating")
+        }
+    }
+    
+    //function to make keyboard disappear when pressing return.
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
+    
+    @IBOutlet weak var RegisterSwitch: UISwitch!
+    
+    //Register Switch Action
+    @IBAction func switchAction(sender: AnyObject) {
+        
+
+        //adding device name to the device object
+        device = PIDevice(name: deviceName.text!)
+        device.type = deviceType.text
+
+        
+        //adding device type to the device object.
+        //NOTE: the type has to exist in the Presence Insights. Default types are : "Internal" and "External"
+        
+        //checks to see if key or value is empty and throw error as necessary
+        if( MissingText(datavalue, key: datakey) ==  false){
+            device.addToDataObject(datavalue.text!, key: datakey.text!)
+        }
+        else{
+            RegisterSwitch.setOn(false, animated: true)
+            alert("Error", messageInput: "Check your Encrypted Data Key and Value Fields")
+        }
+        
+
+        
+        //checks to see if key or value is empty and throw error as necessary for unencrypted data
+        if( MissingText(unencryptedDataValue, key: unencryptedDataKey) ==  false){
+            device.addToUnencryptedDataObject(unencryptedDataValue.text!, key: unencryptedDataKey.text!)
+        }
+        else{
+            RegisterSwitch.setOn(false, animated: true)
+            alert("Error", messageInput: "Check your Unencrypted Data Key and Value Fields")
+        }
+        
+        
+        
+        if RegisterSwitch.on {
+            //PI Device Register SDK call
+            piAdapter.registerDevice(device, callback: { (newDevice, NSError) -> () in
+                // newDevice is of type PIDevice.
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    if(NSError == nil){
+                        self.alert("Success", messageInput: "Successfully Registered the Device")
+                    }
+                    else{
+                        self.alert("Error", messageInput: "\(NSError)")
+                    }
+                })
+            })
+            
+        }
+            
+        else {
+            //PI Device unregister SDK call
+            piAdapter.unregisterDevice(device, callback: { (newDevice, NSError) -> () in
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    if(NSError == nil){
+                        self.alert("Success", messageInput: "Successfully Unregistered the Device")
+                    }
+                    else{
+                        self.alert("Error", messageInput: "\(NSError)")
+                    }
+                })
+            })
+        }
+        
+    }
+    
+    //function to easily create alert messages
+    func alert(titleInput : String , messageInput : String){
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "ok", style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
+
+    }
+    
+    //function to check if both or none of the data value and key exist.
+    func MissingText (value: UITextField, key : UITextField) -> Bool{
+        if (key.text!.isEmpty && value.text!.isEmpty){
+            return false
+        }
+        else if (key.text!.isEmpty || value.text!.isEmpty){
+            return true
+        }
+        else{
+            return false
+        }
+
+    }
+    
+
+    
+   
+    
 }
 
---
+++ README.md
@@ -0,0 +1,68 @@
+#MobileFirst Platform - PI Device Registration 
+The purpose of this app is to show how to implement PI device registration for IOS using Swift.
+
+##Instructions
+You can use IOS device or XCode simulator to run this app.
+
+* Open PIDeviceRegistration.xcodeproj
+* Edit the file ViewController.swift
+	- change the bluemix credentials/information
+		- tenantID, orgID, username and password (all this information can be found in your Presence Insights Dashboard)
+
+* Click the play button to build and run
+
+
+##Quick Walkthrough
+ViewController.swift is a great way to see how objects were initialized and implemented. The app will do an initial check to see if the device is already registered. If the device is registered, it will alert the user and fill the Device name and type.
+
+On the sample app, There are 5 text fields, 1 selection, switch, and update button.
+
+* Device Name - Text Field
+	- Type in the name you want to register the device as.
+* Device Type - Selection
+	- It grabs the device types so the user can select instead of having them type.
+* Encrypted Data Key - Text Field
+	- Encrypted Device Key value
+* Encrypted Data Value - Text Field
+	- Encrypted Device Data value
+* Unencrypted Data Key - Text Field
+    - Unencrypted Device Key value
+* Unencrypted Data Value - Text Field
+    - Unencrypted Device Data value
+	
+*note:* If Key or Value is empty, it will throw an error. Will pass if both exist or empty.
+
+* UI Switch
+	- When the switch is on, it registers the device.
+	- When the switch is off, it unregsiters the device. 
+* Update
+	- will check if the switch is on (regsitered)
+	- If the switch is on, it updates the device.
+
+
+	
+	
+	
+	
+	
+	
+
+
+Copyright 2015 IBM Corp.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+
+
+
+
